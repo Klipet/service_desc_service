b5f9eded4485be5a318469a2a2ee0ac2cd83e0e6
Iura ttttt <iurik1989@gmail.com>
2026-02-20 15:55:53 +0200

    из конфинуратора

--- Core.cs
+++ Core.cs
@@ -1,17 +1,28 @@
 ﻿public class Core
 {
     private static bool UpdatedDatabase = false;
+    private readonly IConfiguration _configuration;
+
     // Initialize the XPO connection
+    public Core(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public void InitializeConnection()
     {
-        string connectionString =
-     "XpoProvider=Postgres;" +
-     "Server=localhost;" +
-     "Port=5432;" +
-     "User ID=postgres;" +
-     "Password=Admin@123;" +
-     "Database=servicedesck;" +
-     "XpoDataStorePool=True;";
+        /*       string connectionString =
+            "XpoProvider=Postgres;" +
+            "Server=localhost;" +
+            "Port=5432;" +
+            "User ID=postgres;" +
+            "Password=Admin@123;" +
+            "Database=servicedesck;" +
+            "XpoDataStorePool=True;";
+               Connect(connectionString);
+       */
+        var connectionString = _configuration.GetConnectionString("XpoConnection")
+            ?? throw new InvalidOperationException("Connection string 'XpoConnection' not found.");
+
         Connect(connectionString);
     }
     private static bool Connect(string connectionString = "")
